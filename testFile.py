b1e52dd1ae82c89e1ebdf33ff5f780706d72b4f9
kevin <majorgrinch@gmail.com>
2019-04-18 23:32:21 -0500

    update ignore

--- testFile.py
+++ testFile.py
@@ -1,9 +1,15 @@
-import core.file_util as cf
-from core.languages import ext_to_lan
+from joern.all import JoernSteps
 
-# d = cf.Directory('/Users/kirk/CTF/program/testPrograms/mongoose/src/')
-# result, file_sum, duration = d.collect_files()
-# print(result)
-a = ext_to_lan
-print(a)
-print(a)
+j = JoernSteps()
+
+j.setGraphDbURL('http://localhost:7474/db/data/')
+
+# j.addStepsDir('Use this to inject utility traversals')
+
+j.connectToDatabase()
+
+res =  j.runGremlinQuery('getFunctionsByName("main")')
+# res =  j.runCypherQuery('...')
+
+for r in res: 
+    print(r)
