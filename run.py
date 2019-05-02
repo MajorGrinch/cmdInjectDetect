8a560181d7a4bb2406216a959c9f6bc65434055a
kevin <majorgrinch@gmail.com>
2019-05-02 16:30:00 -0500

    remove another test file

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-from core.abst import cAST
-cast = cAST('/Users/kirk/homework/spring2019/cse637/class_project/cmdDetect/examples/c_files/exam1.c', line_number=16, function_name='exec')
-ast = cast.ast
-print(cast.check_func_called())
-# for c in ast.ext:
-    # print(type(c))
+import subprocess
+from sys import argv
 
+
+if len(argv) > 1:
+    command = 'joern {0}'.format(argv[1])
+    try:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result, error = p.communicate()
+        print(result)
+    except Exception as e:
+        pass
